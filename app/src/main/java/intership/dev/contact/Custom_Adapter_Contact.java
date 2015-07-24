87f268d347318915d25ef024955caa9e0a92c5d9
chimse <tranthanhvien07dt1@gmail.com>
2015-07-24 10:45:22 +0700

    create adapter_listview..............

---
+++ app/src/main/java/intership/dev/contact/Custom_Adapter_Contact.java
@@ -0,0 +1,110 @@
+package intership.dev.contact;
+
+
+import android.app.AlertDialog;
+import android.app.Fragment;
+
+import android.content.Context;
+import android.content.DialogInterface;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by Administrator on 7/22/2015.
+ */
+public class Custom_Adapter_Contact extends ArrayAdapter<Contacts_Item> {
+    private Context context;
+
+    public Custom_Adapter_Contact(Context context, int resource, ArrayList<Contacts_Item> arr) {
+        super(context, resource, arr);
+        this.context = context;
+    }
+
+    private class ViewHolder {
+        private TextView tvNameContact;
+        private ImageView icContact;
+        private Button btnEdit, btnDelete;
+    }
+
+    ViewHolder holder;
+    Contacts_Item contact_item = null;
+
+    @Override
+    public View getView(final int position, final View convertView, ViewGroup parent) {
+        View myView = convertView;
+        if (convertView == null) {
+            myView = LayoutInflater.from(context).inflate(R.layout.custom_listview_contact, parent, false);
+            holder = new ViewHolder();
+            holder.tvNameContact = (TextView) myView.findViewById(R.id.tvNameContact);
+            holder.icContact = (ImageView) myView.findViewById(R.id.icContact);
+            holder.btnEdit = (Button) myView.findViewById(R.id.btnEditContact);
+            holder.btnDelete = (Button) myView.findViewById(R.id.btnDeleteContact);
+            myView.setTag(holder);
+
+        } else {
+            holder = (ViewHolder) myView.getTag();
+        }
+        contact_item = Constant.array_contact.get(position);
+        holder.icContact.setImageResource(contact_item.getIc_contact());
+        holder.tvNameContact.setText(contact_item.getTvNameContact());
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("Question ? ");
+                builder.setMessage("Bạn Muốn Xóa Liên Hệ Này ?");
+                builder.setIcon(R.drawable.ic_home_trangchu);
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.setPositiveButton("Đúng", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Constant.array_contact.remove(position);
+                        notifyDataSetChanged();
+                    }
+                });
+                builder.create().show();
+            }
+        });
+        holder.btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Constant.DISPLAY_FRAGMENT == 0) {
+                    Fragment fragment=new Fragment_Contacts();
+                    Bundle bd=new Bundle();
+                    bd.putInt("ic",Constant.array_contact.get(position).getIc_contact());
+                    bd.putString("tv", Constant.array_contact.get(position).getTvNameContact());
+                    bd.putString("des",Constant.array_contact.get(position).getDescription());
+                    bd.putInt("position", position);
+                    fragment.setArguments(bd);
+                    MainActivity.fragmentManager.beginTransaction().replace(R.id.frame_container,fragment).commit();
+                    MainActivity.frameLayout.setVisibility(View.VISIBLE);
+                    Constant.DISPLAY_FRAGMENT = 1;
+
+                } else {
+                    MainActivity.frameLayout.setVisibility(View.GONE);
+                    Constant.DISPLAY_FRAGMENT = 0;
+                }
+
+            }
+        });
+
+        return myView;
+    }
+}
